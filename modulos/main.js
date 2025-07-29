2a8f0e6a62f59feb907444a69e76402d2f15c092
kelvin vieira lima <153334603+Klima2204@users.noreply.github.com>
2025-07-28 21:09:28 -0300

    modulos
    exercicio de impot e export

---
+++ modulos/main.js
@@ -0,0 +1,7 @@
+import { soma, dobro } from './funcoes.js'
+
+const resultado1 = soma(5, 3)
+const resultado2 = dobro(4)
+
+console.log(`Resultado da soma: ${resultado1}`)
+console.log(`Resultado do dobro: ${resultado2}`)
